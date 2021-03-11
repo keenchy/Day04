f817d6dca3b0efa494da99d8ce584b1da2235ee0
keenchy <h.kenan81@hotmail.com>
2021-03-11 22:04:28 +0100

    izmjene

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,18 @@
-console.log('Welcome to my first project');
+function main(numOne, numTwo) {
+
+    var quotient;
+    if (numTwo === 0) {
+        quotient === 'Divide by 0! Cannot divide by zero!'
+    } else {
+
+        // Only change code below this line
+
+        var quotient = 15 / 3;
+
+        // Only change code above this line
+    }
+    return quotient;
+}
+
+console.log(main(numOne, numTwo)); // Change this line
+module.exports = main;
