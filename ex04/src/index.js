7c939447970fe0d88115ab8c087dacf26a19b150
keenchy <h.kenan81@hotmail.com>
2021-03-11 22:28:19 +0100

    myDecimal

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+
+    var ourDecimal = 8.2;
+
+    // Only change code below this line
+
+    var myDecimal = 8.2;
+
+    // Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
