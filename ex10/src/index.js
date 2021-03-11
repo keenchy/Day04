47c21573526345dc7320f2e1d8b3a9d8b51d9577
keenchy <h.kenan81@hotmail.com>
2021-03-11 23:14:27 +0100

    adding new index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+
+    var myFirstname = "Kenan";
+    var myLastName = "Hošo";
+    var myHomeTown = "Sarajevo";
+
+
+    return {
+        myFirstname,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
