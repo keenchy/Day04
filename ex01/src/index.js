33947d45c9b6d9e2b34fbf46b00e84d489d56438
keenchy <h.kenan81@hotmail.com>
2021-03-14 18:49:36 +0100

    Dividing one number by another

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,17 +2,20 @@ function main(numOne, numTwo) {
 
     var quotient;
     if (numTwo === 0) {
-        quotient === 'Divide by 0! Cannot divide by zero!'
+        quotient = 'Divide by 0! Cannot divide by zero!';
     } else {
 
         // Only change code below this line
 
-        var quotient = 15 / 3;
+        quotient = numOne / numTwo;
 
         // Only change code above this line
     }
     return quotient;
 }
 
-console.log(main(numOne, numTwo)); // Change this line
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
 module.exports = main;
