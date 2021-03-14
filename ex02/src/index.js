590d35a091077e428b255552ee30bcff06f48672
keenchy <h.kenan81@hotmail.com>
2021-03-14 16:40:12 +0100

    Adding new index.js (Increment ++)

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var myRes = "75";
+
+    // Only change code below this line
+
+    var myRes = myRes++;
+    myRes = myRes + 1;
+
+    // Only change code above this line
+
+    return myRes;
+}
+
+console.log(main());
+module.exports = main;
